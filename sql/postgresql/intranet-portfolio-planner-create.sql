f54ca2bd5af5d9cb99c1d69a7a75bd7e1f58792d
Frank Bergmann <frank.bergmann@project-open.com>
2016-03-23 16:33:13 +0000

    - don't remember

--- sql/postgresql/intranet-portfolio-planner-create.sql
+++ sql/postgresql/intranet-portfolio-planner-create.sql
@@ -27,6 +27,37 @@ SELECT im_menu__new (
 	null						-- p_visible_tcl
 );
 
+SELECT acs_permission__grant_permission(
+	(select menu_id from im_menus where label = 'portfolio_planner'),
+	(select group_id from groups where group_name = 'Employees'),
+	'read'
+);
+
+
+
+SELECT im_menu__new (
+	null,						-- p_menu_id
+	'im_menu',					-- object_type
+	now(),						-- creation_date
+	null,						-- creation_user
+	null,						-- creation_ip
+	null,						-- context_id
+	'intranet-portfolio-planner',			-- package_name
+	'reporting-portfolio-planner',				-- label
+	'Portfolio Planner Report',				-- name
+	'/intranet-portfolio-planner/index',		-- url
+	0,						-- sort_order
+	(select menu_id from im_menus where label = 'reporting-program-portfolio'),
+	null						-- p_visible_tcl
+);
+
+SELECT acs_permission__grant_permission(
+	(select menu_id from im_menus where label = 'reporting-portfolio-planner'),
+	(select group_id from groups where group_name = 'Employees'),
+	'read'
+);
+
+
 -- ------------------------------------------------------
 -- Create a REST data-source for all PMs who have not yet updated their projects
 -- According to the WF
